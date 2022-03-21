3343d4ce166febc24d2e1cae60812065a30d5b55
Nathan <nate77w88@gmail.com>
2022-03-21 11:58:36 -0400

    Updated the models and condensed them into one file, Mongo_Models.js

---
+++ Mongo_Models.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Creates a schematic for a new user being entered into the database
+const newUser = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    userID: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },  
+}, {timestamps: true}, {collection: 'users'});
+
+
+// Used in the workoutMetrics to hold the entire workouts schema
+const workoutExcercises = new Schema({
+    benchpress: {
+        max: Number,
+        presses: [Number]
+    },
+
+    deadlift: {
+        max: Number,
+        presses: [Number]
+    },
+
+    Squat: {
+        max: Number,
+        presses: [Number]
+    }
+});
+
+// Creates the workoutmetrics schema for mongodb
+const workoutMetric = new Schema({
+
+    // Keeps track of whos workoutmetrics this is
+    userID: {
+        type: String,
+        required: true
+    },
+
+    gender: {
+        type: Number, // 1 for Male, 0 for Female
+        required: false
+    },
+
+    weight: {
+        type: Number, 
+        required: false
+    },  
+
+    workouts: workoutExcercises,    // See workoutExcercises above for schema
+    caloriesBurned: Number
+}, {timestamps: true}, {collection: 'workoutmetrics'});
+
+// Creates models for both schemas
+const User = mongoose.model('users', newUser);
+const workoutMets = mongoose.model('workoutmetrics', workoutMetric);
+
+// Exports both modules
+module.exports = {User, workoutMets};
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const User = require('./models/User');
+const {User, workoutMets} = require('./Mongo_Models');
 
 // Allows us to access the .env file
 require("dotenv").config();
@@ -19,17 +19,41 @@ mongoose.connect(mongodb_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 app.get('/add-User', (req, res) => {
+    /*
     const user = new User({
-        firstName: "Bob",
-        lastName: "Tommy",
-        dateOfBirth: "12/12/12"
+        userID: "777",
+        gender: 1,
+        weight: 145,
+        workouts: {
+            benchpress: {
+                max: 400
+            }
+        }
     });
+    */
+    try {
+        const work = new workoutMets({
+            userID: "7000",
+            gender: 1,
+            weight: 145,
+            workouts: {
+                benchpress: {
+                    max: 400,
+                    presses: [123, 455, 788]
+                }
+            }
+        });
 
-    user.save()
+        work.save()
         .then((result) => {
             res.send(result);
         })
         .catch((err) => {
             console.log(err);
         });
+    } catch(e) {
+        console.log(e.message);
+    }
+
+
 })
