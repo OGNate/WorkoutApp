8dd5c2bd27ba5d403168ef2a9fe2da1fdeffcb6b
ronitagarwala01 <34790361+ronitagarwala01@users.noreply.github.com>
2022-03-21 20:14:48 -0400

    Edited login api

--- server.js
+++ server.js
@@ -62,27 +62,25 @@ app.post('/api/login', async (req, res, next) =>
 {
   // incoming: login, password
   // outgoing: id, firstName, lastName, error
-
-  var error = '';
+	
+ var error = '';
 
   const { login, password } = req.body;
+
+  const db = client.db();
+  const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
 
   var id = -1;
   var fn = '';
   var ln = '';
 
-  if( login.toLowerCase() == 'rickl' && password == 'COP4331' )
+  if( results.length > 0 )
   {
-    id = 1;
-    fn = 'Rick';
-    ln = 'Leinecker';
-  }
-  else
-  {
-    error = 'Invalid user name/password';
+    id = results[0].UserId;
+    fn = results[0].FirstName;
+    ln = results[0].LastName;
   }
 
-  var ret = { id:id, firstName:fn, lastName:ln, error:error};
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
   res.status(200).json(ret);
 });
-
