74f2986bbdbb18f6cee7b3eb11eeba19789e5847
Nathan <nate77w88@gmail.com>
2022-03-25 22:32:58 -0400

    Showed how to find a user by their objectId in the /Test endpoint

--- server.js
+++ server.js
@@ -2,103 +2,131 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const {User, workoutMets} = require('./Mongo_Models');
+
+const {
+  User,
+  workoutMets
+} = require('./Mongo_Models');
 
 // Allows us to access the .env file
 require("dotenv").config();
 
+const path = require('path');
+const { ObjectId } = require('mongodb');
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+app.set('port', PORT);
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Gets the mongodb_URI from our .env file and then connects using mongoose to it
 const mongodb_URI = process.env.MONGODB_URI;
-mongoose.connect(mongodb_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => app.listen(process.env.PORT))
-    .catch((err) => console.log(err));
 
-app.post('/register', (req, res) => {
+mongoose.connect(mongodb_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch((err) => console.log(err));
 
-    User.findOne({ email: req.body.email }).then((user) =>{
-        if(user){
-            // if email alread exist throw error
-            return res.status(400).json({ email: "email already exist"})
-        }
-        else{
-            const newUser = new User({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: req.body.password,
-            })
-            newUser.save()
-            return res.status(200).json({msg: newUser})
-        }
-    })
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+app.post('/api/register', (req, res) => {
+
+  User.findOne({
+    email: req.body.email
+  }).then((user) => {
+
+    if (user) {
+
+      return res.status(400).json({
+        email: "email already exist"
+      })
+
+    } else {
+
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+      })
+
+      newUser.save()
+      return res.status(200).json({
+        msg: newUser
+      })
+    }
+  })
 })
 
+// DELETE WHEN DONE
+// Shows how to find a user by their object ID
+app.post('/Test', async (req, res, next) => {
+    User.findById({
+        _id: ObjectId(req.body._id)
+    }).then((user) => {
 
-
-// Creates a 
-app.post('/register', (req, res) => {
-
-    User.findOne({ email: req.body.email }).then((user) =>{
-        if(user){
-            // if email alread exist throw error
-            return res.status(400).json({ email: "email already exist"})
+        if (user) {
+            console.log("Found User");
+            return res.status(200).json({
+                msg: user
+            });
         }
-        else{
-            const newUser = new User({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: req.body.password,
-            })
-            newUser.save()
-            return res.status(200).json({msg: newUser})
-        }
-    })
-})
 
-
-app.post('/api/login', async (req, res, next) => 
-{
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-	
- var error = '';
-
-  const { login, password } = req.body;
-
-  const db = client.db();
-  const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-
-  var id = -1;
-  var fn = '';
-  var ln = '';
-
-  if( results.length > 0 )
-  {
-    id = results[0].UserId;
-    fn = results[0].FirstName;
-    ln = results[0].LastName;
-  }
-
-  var ret = { id:id, firstName:fn, lastName:ln, error:''};
-  res.status(200).json(ret);
+        console.log("invalid User");
+        return res.status(404).json({
+            msg: "Invalid User"
+        });
+    });
 });
 
 
 // Login API
-app.post('/login', async (req, res, next) => 
-{
- 
-    User.findOne({email: req.body.email, password: req.body.password}).then((user) => {
-        if (user) {
-            console.log("Valid");
-            return res.status(200).json({message: "Valid"});
-        }
-        
-        return res.status(400).json({error: "Invalid Credentials"}); 
+app.post('/api/login', async (req, res, next) => {
+
+  User.findOne({
+    email: req.body.email,
+    password: req.body.password
+  }).then((user) => {
+
+    if (user)
+      return res.status(200).json({
+        message: "Valid"
+      });
+
+    return res.status(400).json({
+      error: "Invalid Credentials"
     });
+  });
+});
+
+app.use((req, res, next) => {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on port ' + PORT);
 });
