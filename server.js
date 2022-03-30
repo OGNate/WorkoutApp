1fcfbb7e70802872fc924936fef4bcf3d454712f
Thomas Hanson <thomashansondev@gmail.com>
2022-03-29 20:41:10 -0400

    Heroku deployment (testing)

--- server.js
+++ server.js
@@ -30,16 +30,16 @@ mongoose.connect(mongodb_URI, {
   useUnifiedTopology: true
 }).catch((err) => console.log(err));
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+// // Server static assets if in production
+// if (process.env.NODE_ENV === 'production') {
 
-  // Set static folder
-  app.use(express.static('frontend/build'));
+//   // Set static folder
+//   app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
 
 
 //Register API
