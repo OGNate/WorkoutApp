dc64d8db19df959bd4ffa90f1197e4f0b9b59f8e
Thomas Hanson <thomashansondev@gmail.com>
2022-04-14 20:27:48 -0400

    Updated email verification process for testing purposes.

--- server.js
+++ server.js
@@ -150,22 +150,22 @@ app.post('/api/register', async(req, res) => {
 // Verifies the email of the registered user
 app.get("/emailVerification/:userID/:uniqueEmailToken", async (req, res) => {
 
-  const checkUser = await User.findOne({_id: ObjectId(req.params.userID)});
+  const checkUser = User.findOne({_id: ObjectId(req.params.userID)});
   if(!checkUser) return res.status(420).json({error: "Error at checking userID in server.js email verification"});
 
-  const checkEmailToken = await emailToken.findOne({userID: req.params.userID, token: req.params.uniqueEmailToken});
+  const checkEmailToken = emailToken.findOne({userID: req.params.userID, token: req.params.uniqueEmailToken});
   if(!checkEmailToken) return res.status(420).json({error: "emailToken does not exist"});
 
   console.log("Step 1");
 
   // Changes isVerified classification for the user to true.
   checkUser.isVerified = true;
-  await checkUser.save();
+  checkUser.save();
 
   console.log("Step 2");
 
   // Deletes the email token from emailToken collection
-  await emailToken.deleteOne({userID: ObjectId(req.params.userID), token: req.params.uniqueEmailToken});
+  emailToken.deleteOne({userID: ObjectId(req.params.userID), token: req.params.uniqueEmailToken});
 
   console.log("Step 3");
 
@@ -176,7 +176,7 @@ app.get("/emailVerification/:userID/:uniqueEmailToken", async (req, res) => {
 
   console.log("Step 4");
 
-  await newStat.save();
+  newStat.save();
   
   console.log(`${checkUser.email} is now verified`);
 
